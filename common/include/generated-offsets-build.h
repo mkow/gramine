111eb17df2d99ad1c118a25a0e4505582184e467
Paweł Marczewski <pawel@invisiblethingslab.com>
2021-10-08 12:51:01 +0200

    [LibOS,Pal/Linux-SGX] Generate offsets using an executable
    Instead of generating assembly code and parsing it, we compile an
    executable that outputs desired source (C header file or Python module).
    As a result, the code is less brittle (no need to account for GCC and
    Clang assembly format), and some hacks can be removed from Meson.
    
    Signed-off-by: Paweł Marczewski <pawel@invisiblethingslab.com>

--- common/include/generated-offsets-build.h
+++ common/include/generated-offsets-build.h
@@ -1,10 +1,21 @@
 #ifndef GENERATED_OFFSETS_BUILD_H
 #define GENERATED_OFFSETS_BUILD_H
 
-#define DEFINE(name, value) \
-    __asm__ volatile(".ascii \"GENERATED_INTEGER " #name " %0 \"\n" ::"i"(value))
+#include <stddef.h>
+
+struct generated_offset {
+    const char* name;
+    size_t offset;
+};
+
+extern const struct generated_offset generated_offsets[];
+extern const char* generated_offsets_name;
+
+#define DEFINE(name, value) { #name, value }
 
 #define OFFSET(name, str, member)     DEFINE(name, offsetof(struct str, member))
 #define OFFSET_T(name, str_t, member) DEFINE(name, offsetof(str_t, member))
+
+#define OFFSET_END { NULL, 0 }
 
 #endif /* GENERATED_OFFSETS_BUILD_H */

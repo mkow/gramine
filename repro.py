808cca73ed641414195de6e6ec2c77c5e0fa2c20
Michał Kowalczyk <mkow@invisiblethingslab.com>
2024-04-30 22:12:59 +0200

    fixup! tmp
    Signed-off-by: Michał Kowalczyk <mkow@invisiblethingslab.com>

--- repro.py
+++ repro.py
@@ -21,14 +21,14 @@ data = b'lorem ipsum dolor sit amet consectetur adipiscing elit'
 
 key_path = f'/tmp/pytest_debugging/key_{urandom(16).hex()}.pem'
 print(f'{key_path}')
-with open(key_path, 'wb') as pfile:
-    key = rsa.generate_private_key(public_exponent=SGX_RSA_PUBLIC_EXPONENT,
-        key_size=SGX_RSA_KEY_SIZE, backend=_cryptography_backend)
-
-    private_key = key.private_bytes(encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.PKCS8, encryption_algorithm=serialization.NoEncryption())
-    pfile.write(private_key)
 while True:
+    with open(key_path, 'wb') as pfile:
+        key = rsa.generate_private_key(public_exponent=SGX_RSA_PUBLIC_EXPONENT,
+            key_size=SGX_RSA_KEY_SIZE, backend=_cryptography_backend)
+
+        private_key = key.private_bytes(encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8, encryption_algorithm=serialization.NoEncryption())
+        pfile.write(private_key)
     with open(key_path, 'rb') as key_file:
         exponent, modulus, signature = sign_with_private_key_from_pem_path(data, key_path)
         verify_signature(data, exponent, modulus, signature, key_file)

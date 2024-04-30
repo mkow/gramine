c8b8eca60c74505885ecce8c1d68ef5c4d4ef146
Michał Kowalczyk <mkow@invisiblethingslab.com>
2024-04-30 21:52:19 +0200

    fixup! tmp
    Signed-off-by: Michał Kowalczyk <mkow@invisiblethingslab.com>

---
+++ repro.py
@@ -0,0 +1,36 @@
+from os import urandom
+
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import padding, rsa
+from graminelibos.sgx_sign import (sign_with_private_key_from_pem_path, SGX_RSA_KEY_SIZE, SGX_RSA_PUBLIC_EXPONENT,
+    _cryptography_backend)
+
+def verify_signature(data, exponent, modulus, signature, key_file, passphrase=None):
+    private_key = serialization.load_pem_private_key(key_file.read(), password=passphrase,
+        backend=_cryptography_backend)
+
+    public_key = private_key.public_key()
+
+    numbers = public_key.public_numbers()
+    assert numbers.e == exponent
+    assert numbers.n == modulus
+    signature_bytes = signature.to_bytes((signature.bit_length() + 7) // 8, byteorder='big')
+    public_key.verify(signature_bytes, data, padding.PKCS1v15(), hashes.SHA256())
+
+
+data = b'lorem ipsum dolor sit amet consectetur adipiscing elit'
+
+key_path = f'/tmp/pytest_debugging/key_{urandom(16).hex()}.pem'
+print(f'{key_path}')
+with open(key_path, 'wb') as pfile:
+    key = rsa.generate_private_key(public_exponent=SGX_RSA_PUBLIC_EXPONENT,
+        key_size=SGX_RSA_KEY_SIZE, backend=_cryptography_backend)
+
+    private_key = key.private_bytes(encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8, encryption_algorithm=serialization.NoEncryption())
+    pfile.write(private_key)
+
+with open(key_path, 'rb') as key_file:
+    exponent, modulus, signature = sign_with_private_key_from_pem_path(data, key_path)
+    while True:
+        verify_signature(data, exponent, modulus, signature, key_file)

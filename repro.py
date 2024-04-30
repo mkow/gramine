dd4d83780f1800905ff83d9cde36f5846f5891ea
Michał Kowalczyk <mkow@invisiblethingslab.com>
2024-04-30 22:40:36 +0200

    fixup! tmp
    Signed-off-by: Michał Kowalczyk <mkow@invisiblethingslab.com>

--- repro.py
+++ repro.py
@@ -1,23 +1,13 @@
 from os import urandom
 
-from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding, rsa
-from graminelibos.sgx_sign import (sign_with_private_key_from_pem_path, SGX_RSA_KEY_SIZE, SGX_RSA_PUBLIC_EXPONENT,
-    _cryptography_backend, sign_with_private_key)
+from graminelibos.sgx_sign import _cryptography_backend, sign_with_private_key
 
-def verify_signature(data, signature, key_file, passphrase=None):
-    private_key = serialization.load_pem_private_key(key_file.read(), password=passphrase,
-        backend=_cryptography_backend)
-
-    public_key = private_key.public_key()
-
-    signature_bytes = signature.to_bytes((signature.bit_length() + 7) // 8, byteorder='big')
-    public_key.verify(signature_bytes, data, padding.PKCS1v15(), hashes.SHA256())
-
+SGX_RSA_PUBLIC_EXPONENT = 3
+SGX_RSA_KEY_SIZE = 3072
 data = b'lorem ipsum dolor sit amet consectetur adipiscing elit'
 
-key_path = f'/tmp/pytest_debugging/key_{urandom(16).hex()}.pem'
-print(f'{key_path}')
 while True:
     print('.', end='', flush=True)
     key = rsa.generate_private_key(public_exponent=SGX_RSA_PUBLIC_EXPONENT,

3d9b85e6a8e710f7ecd203f3a39f347f3aca2586
Michał Kowalczyk <mkow@invisiblethingslab.com>
2024-04-30 22:47:43 +0200

    fixup! tmp
    Signed-off-by: Michał Kowalczyk <mkow@invisiblethingslab.com>

--- repro.py
+++ repro.py
@@ -1,17 +1,17 @@
 from os import urandom
-
+from cryptography.hazmat import backends
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding, rsa
-from graminelibos.sgx_sign import _cryptography_backend, sign_with_private_key
+from graminelibos.sgx_sign import sign_with_private_key
 
 SGX_RSA_PUBLIC_EXPONENT = 3
 SGX_RSA_KEY_SIZE = 3072
-data = b'lorem ipsum dolor sit amet consectetur adipiscing elit'
+data = b'dupa'
 
 while True:
     print('.', end='', flush=True)
     key = rsa.generate_private_key(public_exponent=SGX_RSA_PUBLIC_EXPONENT,
-        key_size=SGX_RSA_KEY_SIZE, backend=_cryptography_backend)
+        key_size=SGX_RSA_KEY_SIZE, backend=backends.default_backend())
 
     _, _, signature = sign_with_private_key(data, key)
     public_key = key.public_key()

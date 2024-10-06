a5c9f4e6280a6f7ad75bd50eb97104bc88332dc1
Michał Kowalczyk <mkow@invisiblethingslab.com>
2024-10-06 16:16:30 +0200

    fixup! tmp
    Signed-off-by: Michał Kowalczyk <mkow@invisiblethingslab.com>

--- pal/src/host/linux/gdb_integration/gramine_linux_gdb.py
+++ pal/src/host/linux/gdb_integration/gramine_linux_gdb.py
@@ -9,9 +9,9 @@ def main():
     common_path = '../../gdb_integration/'
     for filename in [
             common_path + 'language_gdb.py',
-            common_path + 'pagination_gdb.py',
-            common_path + 'debug_map_gdb.py',
-            common_path + 'gramine.gdb',
+            # common_path + 'pagination_gdb.py',
+            # common_path + 'debug_map_gdb.py',
+            # common_path + 'gramine.gdb',
     ]:
         print("[%s] Loading %s..." % (os.path.basename(__file__), filename))
         path = os.path.join(os.path.dirname(__file__), filename)

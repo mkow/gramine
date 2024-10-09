32dfeac3a81b43bf82bd4b815d7041c2fc2d0e8e
Michał Kowalczyk <mkow@invisiblethingslab.com>
2024-10-10 01:02:15 +0200

    fixup! tmp
    Signed-off-by: Michał Kowalczyk <mkow@invisiblethingslab.com>

--- pal/src/host/linux-sgx/host_ocalls.c
+++ pal/src/host/linux-sgx/host_ocalls.c
@@ -697,7 +697,10 @@ static long sgx_ocall_debug_map_remove(void* args) {
 static long sgx_ocall_debug_describe_location(void* args) {
     struct ocall_debug_describe_location* ocall_debug_args = args;
 
-    // malloc(30);
+    static size_t cnt = 0;
+    cnt++;
+    if (cnt > 30)
+        malloc(30);
     return -ENOSYS;
 // #ifdef DEBUG
 //     return debug_describe_location(ocall_debug_args->addr, ocall_debug_args->buf,

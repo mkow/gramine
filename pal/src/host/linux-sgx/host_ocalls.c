a1f825b44a450cf584b93510b62e2dd360e3897b
Michał Kowalczyk <mkow@invisiblethingslab.com>
2024-10-10 01:08:02 +0200

    fixup! tmp
    Signed-off-by: Michał Kowalczyk <mkow@invisiblethingslab.com>

--- pal/src/host/linux-sgx/host_ocalls.c
+++ pal/src/host/linux-sgx/host_ocalls.c
@@ -699,7 +699,7 @@ static long sgx_ocall_debug_describe_location(void* args) {
 
     static size_t cnt = 0;
     cnt++;
-    if (cnt > 300)
+    if (cnt > 3000)
         malloc(30);
     return -ENOSYS;
 // #ifdef DEBUG
